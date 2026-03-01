f656e83644705db188079acd2f9b0e0243a743db
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-03-01 21:45:55 +0530

    Initialize Spring Boot application

--- src/main/java/com/my_project/ReservationApplication.java
+++ src/main/java/com/my_project/ReservationApplication.java
@@ -1,4 +1,12 @@
 package com.my_project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationApplication.class, args);
+    }
 }
