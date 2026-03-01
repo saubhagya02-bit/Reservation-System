3d0ed8c30eedd837005c48c655a9a4fae2014b0b
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-03-01 22:44:47 +0530

    Update controllers to use DTOs

--- src/main/java/com/my_project/controller/UserController.java
+++ src/main/java/com/my_project/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.my_project.controller;
 
+import com.my_project.dto.UserDTO;
 import com.my_project.model.User;
 import com.my_project.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,8 @@ public class UserController {
     }
 
     @PostMapping
-    public User create(@RequestBody User user) {
+    public User create(@Valid @RequestBody UserDTO userDTO) {
+        User user = new User(userDTO.getName(), userDTO.getEmail());
         return repository.save(user);
     }
 
